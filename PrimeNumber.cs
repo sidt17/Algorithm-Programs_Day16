8335cb6384f0bc5f5ac2210dfad07ff070f62d89
Siddhant <santoshshrivastava39@gmail.com>
2022-03-29 22:48:17 +0530

    Prime Numbers-UC7

---
+++ PrimeNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class PrimeNumber
+    {
+        public void CheckPrimeNumber()
+        {
+            bool isPrime = true;
+            Console.WriteLine("Prime Numbers : ");
+            for (int i = 2; i <= 1000; i++)
+            {
+                for (int j = 2; j <= 1000; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime)
+                {
+                    Console.Write("\t" + i);
+                }
+                isPrime = true;
+            }
+        }
+    }
+}
