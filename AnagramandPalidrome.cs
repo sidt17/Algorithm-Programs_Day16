4edac06e37d26db530bdbea4c68561527bdcdb2a
Siddhant <santoshshrivastava39@gmail.com>
2022-03-29 23:02:03 +0530

    Anagram and Palindrome

---
+++ AnagramandPalidrome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class AnagramandPalindrome
+    {
+
+
+        public void Check()
+        {
+            int n, r, sum = 0, temp;
+            Console.Write("Enter the Number: ");
+            n = int.Parse(Console.ReadLine());
+            temp = n;
+            while (n > 0)
+            {
+                r = n % 10;
+                sum = (sum * 10) + r;
+                n = n / 10;
+            }
+            if (temp == sum)
+                Console.Write("Number is Palindrome.");
+            else
+                Console.Write("Number is not Palindrome");
+        }
+    }
+}
