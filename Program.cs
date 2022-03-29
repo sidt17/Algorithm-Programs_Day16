2cbc76c2c61ffcb58db5cce967fd9e55514d5afe
Siddhant <santoshshrivastava39@gmail.com>
2022-03-29 16:27:50 +0530

    Call in main method

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AlgorithmPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Permutation obj = new Permutation();
+            String str = "SID";
+            int k = str.Length;
+            obj.Permute(str, 0, k - 1);
+
+        }
+    }
+
+
+}
