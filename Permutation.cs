b0c3eec80f4916665aa95173d70100bae3623a21
Siddhant <santoshshrivastava39@gmail.com>
2022-03-29 16:29:43 +0530

    Permutation

---
+++ Permutation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmPrograms
+{
+    class Permutation
+    {
+        public void Permute(String str, int l, int r)
+        {
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = Swap(str, l, i);
+                    Permute(str, l + 1, r);
+                    str = Swap(str, l, i);
+                }
+            }
+        }
+
+        public String Swap(String a, int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string e = new string(charArray);
+            return e;
+        }
+
+    }
+}
