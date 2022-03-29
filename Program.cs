a9284b47185c60e702761445459fe7dc6f60a4f9
Siddhant <santoshshrivastava39@gmail.com>
2022-03-29 22:47:35 +0530

    Call in main method

--- Program.cs
+++ Program.cs
@@ -1,34 +1,23 @@
 ﻿using System;
-namespace AnagramDemo
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            string str1 = "heater";
-            string str2 = "reheat";
+           
 
-            char[] ch1 = str1.ToLower().ToCharArray();
-            char[] ch2 = str2.ToLower().ToCharArray();
+            
+            PrimeNumber primeNumbers = new PrimeNumber();
+            primeNumbers.CheckPrimeNumber();
 
-            Array.Sort(ch1);
-            Array.Sort(ch2);
-
-            string val1 = new string(ch1);
-            string val2 = new string(ch2);
-
-            Console.WriteLine(val1);
-            Console.WriteLine(val2);
-
-            if (val1 == val2)
-            {
-                Console.WriteLine("Anagram");
-
-            }
-            else
-            {
-                Console.WriteLine("Not Anagram");
-            }
+            
+           
         }
     }
 }
